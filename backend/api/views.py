facc7aa9fea2135fe327a7354c7e39439d8ad1cb
Misha Gupta <msg0022@outlook.com>
2025-08-14 23:27:06 -0400

    update ping endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -156,7 +156,7 @@ class TeacherSearchView(generics.ListAPIView):
     filterset_class = TeacherFilter
     queryset = Teacher.objects.all().order_by('id')
 
-@api_view(['GET'])
+@api_view(['GET', 'HEAD', 'POST'])
 @permission_classes([AllowAny])
 def ping(request):
     return Response(status=status.HTTP_200_OK)
